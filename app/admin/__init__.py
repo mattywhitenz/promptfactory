7d929776bff17e19511e47fc4609f32c2e913d32
mattywhitenz <128647429+mattywhitenz@users.noreply.github.com>
2025-07-11 17:15:32 +1000

    Delete app directory

--- app/admin/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('admin', __name__, url_prefix='/admin')
-
-# Import routes first, then super_admin to avoid conflicts
-from app.admin import routes, super_admin
